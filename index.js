ea92e5061b269ef3ae5d87bfef3f0a09aff9c782
Benedict Wong <benedictwcy@hotmail.com>
2022-04-23 17:42:34 +0800

    Added general page layout

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
+// =================================================
+// ============== Import Dependencies ==============
+// =================================================
 const express = require('express');
 const hbs = require('hbs');
 const wax = require('wax-on');
 
 require('dotenv').config();
 
-// create an instance of express app
+// =================================================
+// ===== Express Application Setup & Middleware ====
+// =================================================
 let app = express();
-
 // set the view engine
-app.set('view-engine', 'hbs');
-
+app.set('view engine', 'hbs');
 // static folder
 app.use(express.static('public'));
-
 // setup wax-on 
 wax.on(hbs.handlebars)
 wax.setLayoutPath('./views/layouts');
-
 // enable forms
 app.use(express.urlencoded({extended: false}))
 
+// =================================================
+// =============== Route Middlewares ===============
+// =================================================
+app.use((req, res, next) => {
+    res.locals.year = new Date().getFullYear()
+    next()
+})
+
+// =================================================
+// ================= Import Routes =================
+// =================================================
+const routes = {
+    landing: require('./routes/landing'),
+    login: require('./routes/login'),
+    products: require('./routes/products')
+}
+
 async function main() {
-    app.get('/', (req, res) => {
-        res.send('Hello world!')
-    })
+    app.use('/', routes.landing)
+    app.use('/login', routes.login)
+    app.use('/products', routes.products)
 }
 
 main()
