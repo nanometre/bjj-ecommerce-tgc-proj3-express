acb2a9f838911781656cde67e69de0645a8d8dfb
Benedict Wong <benedictwcy@hotmail.com>
2022-05-12 22:18:22 +0800

    Added api route for product search

--- routes/api/products.js
+++ routes/api/products.js
@@ -4,6 +4,7 @@
 const express = require('express')
 const router = express.Router()
 const productDataLayer = require('../../dal/products')
+const { Product } = require('../../models')
 
 // =================================================
 // ================== Set Routes ===================
@@ -11,6 +12,69 @@ const productDataLayer = require('../../dal/products')
 router.get('/', async (req, res) => {
     const products = await productDataLayer.getProducts()
     res.send(products)
+})
+
+router.post('/', async (req, res) => {
+    const q = Product.collection()
+    if (Object.keys(req.body).length === 0) {
+        const products = await q.fetch({
+            withRelated: ['material', 'weave', 'category', 'brand', 'variants']
+        })
+        res.send(products)
+    } 
+    else if (Object.keys(req.body).length != 0) {
+        if (req.body.name) {
+            q.where('product_name', 'ilike', '%' + req.body.name + '%')
+        }
+        if (req.body.min_cost) {
+            q.where('cost', '>=', (req.body.min_cost * 100).toString())
+        }
+        if (req.body.max_cost) {
+            q.where('cost', '<=', (req.body.max_cost * 100).toString())
+        }
+        if (req.body.min_weight) {
+            q.where('weight', '>=', req.body.min_weight)
+        }
+        if (req.body.max_weight) {
+            q.where('weight', '<=', req.body.max_weight)
+        }
+        if (req.body.material_id) {
+            q.where('material_id', '=', req.body.material_id)
+        }
+        if (req.body.weave_id) {
+            q.where('weave_id', '=', req.body.weave_id)
+        }
+        if (req.body.category_id) {
+            q.where('category_id', '=', req.body.category_id)
+        }
+        if (req.body.brand_id) {
+            q.where('brand_id', '=', req.body.brand_id)
+        }
+        const products = await q.fetch({
+            withRelated: ['material', 'weave', 'category', 'brand']
+        })
+        res.send(products)
+    }
+})
+
+router.get('/materials', async (req, res) => {
+    const materials = await productDataLayer.getAllMaterials()
+    res.send(materials)
+})
+
+router.get('/weaves', async (req, res) => {
+    const weaves = await productDataLayer.getAllWeaves()
+    res.send(weaves)
+})
+
+router.get('/categories', async (req, res) => {
+    const categories = await productDataLayer.getAllCategories()
+    res.send(categories)
+})
+
+router.get('/brands', async (req, res) => {
+    const brands = await productDataLayer.getAllBrands()
+    res.send(brands)
 })
 
 router.get('/:product_id/variants', async (req, res) => {
